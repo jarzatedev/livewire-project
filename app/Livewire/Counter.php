f2465c1eed17853eda05a22b49194919863ac00c
Jonathan Arzate Juarez <dev.jonathanarzate@gmail.com>
2023-11-01 09:51:05 -0600

    #FEATURE:First component Add first livewiere component with functionality.

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -2,18 +2,26 @@
 
 namespace App\Livewire;
 
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Livewire\Component;
 
 class Counter extends Component
 {
     public $count = 0;
 
-    public function increment()
+    public function increment(): void
     {
         $this->count++;
     }
 
-    public function render()
+    public function decrement(): void
+    {
+        $this->count--;
+    }
+
+    public function render(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('livewire.counter');
     }
